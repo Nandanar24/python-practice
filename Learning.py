916aa6f621655db42a694e560f05a43a785f650f
Nandana Ramachandran <nandanaramachandran@Nandanas-MacBook-Air.local>
2026-04-08 12:36:42 +1200

    first python program

---
+++ Learning.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello", name)
+if age < 18:
+    print("You are under 18")
+else:
+    print("You are 18 or older")
+    git remote add origin https://github.com/Nandana24/python-practice.git
+git branch -M main
+git push -u origin main
