bded38148c3e29a0b5109e13f2a7124d7d7e0f47
nandanar24 <nandanar24@gmail.com>
2026-04-09 11:41:20 +1200

    Made a game

--- Learning.py
+++ Learning.py
@@ -2,6 +2,6 @@ import random
 number = random.randint(1, 10)
 guess = int(input("Guess a number between 1 and 10: "))
 if guess == number:
-    print("OMG you got it 😭🔥")
+    print("Good Job You Guessed it")
 else:
     print("Nope 💀 the number was", number)
